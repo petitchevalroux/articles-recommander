67361b91ac79aec8a368cb624b2292e7326c8fd0
Patrick Poulain <petitchevalroux@free.fr>
2017-02-18 09:01:57 +0100

    Add count when fetching recommendations

--- src/front/widget/widget.js
+++ src/front/widget/widget.js
@@ -5,8 +5,10 @@
 (function artRecLib(window) {
     var lib = {
         "slots": [],
+        "recommendationsCount": 0,
         "addSlot": function(target, count) {
-            console.log(target, count);
+            this.recommendationsCount = this.recommendationsCount +
+                Math.max(0, parseInt(count));
         },
         "start": function() {
             this.fetchRecommendations(document.location.href);
@@ -15,14 +17,21 @@
             var script = document.createElement("script");
             script.setAttribute(
                 "src",
-                this.getRecommendationsUrl(to)
+                this.getRecommendationsUrl(
+                    to,
+                    this.getRecommendationsCount()
+                )
             );
             document.head.appendChild(script);
         },
         "loadRecommendations": function() {},
-        "getRecommendationsUrl": function(to) {
-            return "/recommendations.js?to=" + encodeURIComponent(
-                to);
+        "getRecommendationsUrl": function(to, count) {
+            return "/recommendations.js?" +
+                "to=" + encodeURIComponent(to) +
+                "&count=" + encodeURIComponent(count);
+        },
+        "getRecommendationsCount": function() {
+            return this.recommendationsCount;
         }
     };
     try {
