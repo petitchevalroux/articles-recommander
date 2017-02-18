65e4d966b300a5bcacae3fe8a35db47e1a52f539
Patrick Poulain <petitchevalroux@free.fr>
2017-02-18 08:40:58 +0100

    Load recommendations from main.js

--- src/front/widget/widget.js
+++ src/front/widget/widget.js
@@ -4,17 +4,34 @@
 
 (function artRecLib(window) {
     var lib = {
+        "slots": [],
         "addSlot": function(target, count) {
             console.log(target, count);
         },
-        "start": function() {}
+        "start": function() {
+            this.fetchRecommendations(document.location.href);
+        },
+        "fetchRecommendations": function(to) {
+            var script = document.createElement("script");
+            script.setAttribute(
+                "src",
+                this.getRecommendationsUrl(to)
+            );
+            document.head.appendChild(script);
+        },
+        "loadRecommendations": function() {},
+        "getRecommendationsUrl": function(to) {
+            return "/recommendations.js?to=" + encodeURIComponent(
+                to);
+        }
     };
     try {
         var slots = window.artRecSlots;
         slots.forEach(function(options) {
             lib.addSlot(options.target, options.count);
         });
-        lib.start();
+        window.artRecLib = lib;
+        window.artRecLib.start();
     } catch (e) {
         console.log("artrec error: " + e);
     }
