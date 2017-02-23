d5b917938cec1e9cbcb0a8b921324bf8fb259d9b
Patrick Poulain <petitchevalroux@free.fr>
2017-02-23 10:06:20 +0100

    Render default template using dot

--- src/widget/front/widget.js
+++ src/widget/front/widget.js
@@ -1,5 +1,6 @@
 /* eslint-env browser */
 /* eslint no-console: 0 */
+/* global doT:false */
 "use strict";
 
 (function artRecLib(window) {
@@ -9,6 +10,10 @@
         "addSlot": function(target, count) {
             this.recommendationsCount = this.recommendationsCount +
                 Math.max(0, parseInt(count));
+            this.slots.push({
+                "target": target,
+                "count": count
+            });
         },
         "start": function() {
             this.fetchRecommendations(document.location.href);
@@ -25,7 +30,27 @@
             document.head.appendChild(script);
         },
         "loadRecommendations": function(recommendations) {
-            console.log(recommendations);
+            var start = 0;
+            var self = this;
+            this.slots.forEach(function(slot) {
+                var slotRecommendations = [];
+                while (slotRecommendations.length < slot.count) {
+                    var end = Math.min(start + slot.count,
+                        recommendations.length);
+                    recommendations.slice(start, end)
+                        .forEach(function(r) {
+                            slotRecommendations.push(r);
+                            start = (start + 1) %
+                                recommendations.length;
+                        });
+                }
+                self.renderSlot(
+                    slot.target,
+                    self.getSlotTemplate(slot),
+                    slotRecommendations
+                );
+            });
+
         },
         "getRecommendationsUrl": function(to, count) {
             return "/recommendations.js?" +
@@ -34,6 +59,21 @@
         },
         "getRecommendationsCount": function() {
             return this.recommendationsCount;
+        },
+        "renderSlot": function(target, template, recommendations) {
+            document.getElementById(target)
+                .innerHTML = template(recommendations);
+        },
+        "getSlotTemplate": function(slot) {
+            var tpl = "";
+            if (!slot.template) {
+                tpl = "<table>{{~it :recommendation:index}}<tr>" +
+                    "<td><a href=\"{{=recommendation.url}}\"><img src=\"{{=recommendation.image}}\"></a></td>" +
+                    "<td><a href=\"{{=recommendation.url}}\">{{=recommendation.title}}</a></td></tr>{{~}}</table>";
+            } else {
+                tpl = slot.template;
+            }
+            return doT.template(tpl);
         }
     };
     try {
