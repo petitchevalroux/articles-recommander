c437f6f00dffa8fa9dd1b08b258a61a346097ca6
Patrick Poulain <petitchevalroux@free.fr>
2017-03-20 22:18:22 +0100

    Move continous queries to dedicated file

--- influxql/continuous-queries.sql
+++ influxql/continuous-queries.sql
@@ -1,22 +1,3 @@
-# articles-recommander
-Articles recommander platform
-
-## Datastore
-Datastore is a [jsonapi client](https://github.com/holidayextras/jsonapi-client) instance.
-
-### Launch redis server using docker
-```
-docker run -p 6379:6379 --name redis -d redis
-```
-
-## Launch influx serveur using docker
-```
-docker run -p 8083:8083 -p 8086:8086 --name influxdb -d influxdb
-```
-
-## Influx continues queries
-daily_events contains click and display per day
-```
 DROP CONTINUOUS QUERY "daily_events_display" ON "articles-recommender";
 CREATE CONTINUOUS QUERY "daily_events_display" ON "articles-recommender"
 RESAMPLE EVERY 1h FOR 1d
@@ -29,12 +10,9 @@ RESAMPLE EVERY 1h FOR 1d
 BEGIN
   SELECT sum(count) as click INTO daily_events FROM events where action='click' group by time(1d)
 END;
-
 DROP MEASUREMENT "daily_events";
 SELECT sum(count) as display INTO daily_events FROM events where action='display' and time < now() - 1d group by time(1d);
 SELECT sum(count) as click INTO daily_events FROM events where action='click' and time < now() - 1d group by time(1d);
-
-
 DROP CONTINUOUS QUERY "articles_click" ON "articles-recommender";
 CREATE CONTINUOUS QUERY "articles_click" ON "articles-recommender"
 RESAMPLE EVERY 1d FOR 1d
@@ -51,7 +29,3 @@ END;
 DROP MEASUREMENT "articles";
 select sum(count) as click into articles from events where action='click' and time < now() - 1d group by time(1d),value;
 select sum(count) as display into articles from events where action='display' and time < now() - 1d group by time(1d),value;
-```
-
-## Todo
- * Check if article exists before inserting in bin/fetch-articles.js
