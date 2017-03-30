224977ab6a87fe0f6d0458d8cf39ccf1825bf676
Patrick Poulain <petitchevalroux@free.fr>
2017-03-30 23:46:20 +0200

    Use least displayed article ids for recommendations

--- src/models/recommendations.js
+++ src/models/recommendations.js
@@ -1,6 +1,7 @@
 "use strict";
 var path = require("path");
 var di = require(path.join(__dirname, "..", "di"));
+var Promise = require("bluebird");
 
 function RecommendationsModel() {}
 
@@ -10,7 +11,45 @@ function RecommendationsModel() {}
  * @returns {Promise}
  */
 RecommendationsModel.prototype.getArticlesIds = function(count) {
-    return di.articlesModel.getRandomIds(count);
+    var fetches = [
+        function() {
+            return di
+                .articlesStatsModel
+                .getLeastDisplayIds(count);
+        },
+        function() {
+            return di
+                .articlesModel
+                .getRandomIds(count);
+        }
+    ];
+    var results = {};
+    return new Promise(function(resolve) {
+        var nextFetch = function(index) {
+            fetches[index]()
+                .then(function(ids) {
+                    ids.forEach(function(id) {
+                        if (!results[id]) {
+                            results[id] = true;
+                            count--;
+                        }
+                    });
+                    if (count > 0 && index < fetches.length) {
+                        nextFetch(index + 1);
+                    } else {
+                        resolve(
+                            Object.getOwnPropertyNames(
+                                results)
+                        );
+                    }
+                    return ids;
+                })
+                .catch(function(error) {
+                    di.log.error(new di.Error(error));
+                });
+        };
+        nextFetch(0);
+    });
 };
 
 module.exports = RecommendationsModel;
